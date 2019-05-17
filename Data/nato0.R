1fd72c67ee206bfd98029dd96ce4edfa68125797
Eric S. Cohen <escohen@uw.edu>
2019-05-17 06:07:19 -0700

    Restored R script file (not essential to assignment)

---
+++ Data/nato0.R
@@ -0,0 +1,11 @@
+data <- read.csv("athlete_events.csv", stringsAsFactors=FALSE)
+#data$Medal[is.na(data$Medal)]<-"None"
+#data[is.na(data)]<-0
+print(length(unique(data$Sport)))
+write.table(data, "athlete_events0.csv", row.names=FALSE)
+weight <- data$Weight[!is.na(data$Weight)]
+height <- data$Height[!is.na(data$Height)]
+#max(height)
+#min(height)
+#max(width)
+#min(width)
