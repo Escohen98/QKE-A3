1ac11385414a26364fbbe5e927f7f031c5b2b967
Eric S. Cohen <escohen@uw.edu>
2019-05-16 23:40:28 -0700

    Still as lost as ever.

--- Frontend/hwa.js
+++ Frontend/hwa.js
@@ -1,13 +1,20 @@
-d3.csv('../Data/athlete_events.csv', function(error, data) {
+d3.csv('../Data/athlete_events0.csv', function(error, data) {
+	data.forEach(function(d) {
+			countries = d.Team.split("-");
+			d.country = countries[0];
+			d.year = +d.Year;
+			d.age = +d.Age;
+			d.season = +d.Season;
+			d.weight = +d.Weight;
+			d.medal = +d.Medal;
+			d.height = +d.Height;
+			d.sex = +d.Sex;
+			return d;
+		})
 
 	var margin = {top: 25, bottom: 10, left: 25, right: 25},
 	width = 700 - margin.left - margin.right,
 	height = 400 - margin.top - margin.bottom;
-
-	data.filter(function(d) {
-		return d.Weight == "NA" || d.Age == "NA" || d.Height == "NA";
-	})
-	.remove();
 
 	var svg = d3.select("body").append("svg")
 	    .attr("width", width + margin.left + margin.right)
@@ -40,25 +47,25 @@ d3.csv('../Data/athlete_events.csv', function(error, data) {
 			.attr("class", "y-axis")
 			.call(yAxis)
 
-		var Years = [...new Set(data.map(function(d) { return d.Year; }))]
-
-		var options = d3.select("#Year").selectAll("option")
-			.data(Years)
+		var years = [...new Set(data.map(function(d) { return d.year; }))]
+		console.log(years)
+		var options = d3.select("#year").selectAll("option")
+			.data(years)
 		.enter().append("option")
 			.text(function(d) {return d;})
 
-		var select = d3.select("#Year")
+		var select = d3.select("#year")
 			.on("change", function() {
-				update(data, this.Weight)
+				update(data, this.medal)
 			})
 
-		update(data, d3.select("#Year").property("Weight"))
+		update(data, d3.select("#year").property("medal"))
 
-		function update(data, Years) {
-			var data = data.filter(function(d) { return d.Year == Years})
+		function update(data, years) {
+			var data = data.filter(function(d) { return d.year == years})
 
-			x.domain(data.map(function(d) {return d.Team;}))
-			y.domain([0, d3.max(data, function(d) {return d.Weight;})]).nice()
+			x.domain(data.map(function(d) {return d.country;}))
+			y.domain([0, d3.max(data, function(d) {return d.medal;})]).nice()
 
 			svg.selectAll(".x-axis")
 				.transition()
@@ -71,7 +78,7 @@ d3.csv('../Data/athlete_events.csv', function(error, data) {
 				.call(yAxis);
 
 			var bar = svg.selectAll(".bar")
-				.data(data, function(d) {return d.Team;})
+				.data(data, function(d) {return d.country;})
 
 			bar.exit().remove();
 
@@ -81,8 +88,9 @@ d3.csv('../Data/athlete_events.csv', function(error, data) {
 				.attr("width", x.bandwidth())
 				.merge(bar)
 				.transition().duration(1000)
-				.attr("x", function(d) { return x(d.Team)})
-				.attr("y", function(d) { return y(d.Weight)})
-				.attr("height", function(d) { return y(0) - y(d.Weight)})
+				.attr("x", function(d) { return x(d.country)})
+				.attr("y", function(d) { return y(d.medal)})
+				.attr("height", function(d) { return y(0) - y(d.medal)})
 			}
+
 		})
